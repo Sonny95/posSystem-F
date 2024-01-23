e535e77766894f8a184d6e596e69b548bd12b024
Sonny95 <thswlgml9598@gmail.com>
2024-01-23 20:47:46 +1100

    Revert "API for backend"
    This reverts commit 1a28bbff4d3085eedd4ed25cd4fe2860f8506046.

--- src/pages/api/updateStatus/[updateId].js
+++ src/pages/api/updateStatus/[updateId].js
@@ -1,10 +1,7 @@
 import connection from "./../db";
 
 export default (req, res) => {
-  console.log(req.params, "req");
-  //TODO dynamic API changed it to query.
   const orderId = req.params.updateId;
-  console.log(req.body, "req.body");
   const newStatus = req.body.status;
 
   const updateStatusQuery = "UPDATE `order` SET status = ? WHERE id = ?";
