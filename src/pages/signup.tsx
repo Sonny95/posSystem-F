c2f30e9333f3a4ac5d9cf7d990ba6fc42ef8320e
Sonny95 <thswlgml9598@gmail.com>
2024-01-31 20:56:57 +1100

    sign up

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,12 +1,9 @@
 import Link from "next/link";
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function Signup() {
-  const [code, setCode] = useState("");
-  const [name, setName] = useState("");
-  const [number, setNumber] = useState("");
-  const [password, setPassword] = useState("");
-
+  const { register } = useForm();
   return (
     <div>
       <div className="w-full h-full flex items-center justify-center bg-gray-100">
@@ -14,21 +11,25 @@ function Signup() {
           <img src="logo.png" className="mb-[30px]" />
           <div className="mb-[5px] text-2xg">Business Code</div>
           <input
+            {...register("code", { required: true, maxLength: 10 })}
             name="code"
             className="w-[250px] h-[50px] bg-gray-200 mb-[30px] rounded-lg"
           ></input>
           <div className="mb-[5px] text-2xg">Name</div>
           <input
+            {...register("Staff Name", { required: true, maxLength: 16 })}
             name="name"
             className="w-[250px] h-[50px] bg-gray-200 mb-[30px] rounded-lg"
           ></input>
           <div className="mb-[5px] text-2xg">Store Number</div>
           <input
+            {...register("number", { required: true, maxLength: 10 })}
             name="number"
             className="w-[250px] h-[50px] bg-gray-200 mb-[30px] rounded-lg"
           ></input>
           <div className="mb-[5px] text-2xg">Password</div>
           <input
+            {...register("password", { required: true, minLength: 8, maxLength: 16 })}
             name="password"
             className="w-[250px] h-[50px] bg-gray-200 mb-[30px] rounded-lg"
           ></input>
